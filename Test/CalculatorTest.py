76d84f7eb4cd29e98b598776d8a2fb50368581ca
edisonospina16 <edison100ospina@gmail.com>
2024-08-16 07:36:05 -0500

    raisses

--- Test/CalculatorTest.py
+++ Test/CalculatorTest.py
@@ -1,6 +1,9 @@
 import unittest
 import Test_Error
 
+import unittest
+
+
 class ParametrosPension:
     def __init__(self):
         self.edad = None
@@ -9,309 +12,315 @@ class ParametrosPension:
         self.ahorro_pensional_a_hoy = None
         self.rentabilidad_promedio = None
         self.tasa_administracion = None
-        self.edad_pension_total = None
 
 
 class PensionTest(unittest.TestCase):
-    # casos normales (6)
+
+    def setUp(self):
+        # Configuración inicial común a todas las pruebas
+        self.parametros = ParametrosPension()
+
+    # Casos normales (6)
     def test_caso_normal1(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 25
+        # Entradas
+        self.parametros.edad = 25
         sexo = "m"
         estado_civil = "s"
-        parametros.salario_actual = 1500000
-        parametros.semanas_laboradas = 200
-        parametros.ahorro_pensional_a_hoy = 1000000
-        parametros.rentabilidad_promedio = 4
-        parametros.tasa_administracion = 1
-        # salidas esperadas
+        self.parametros.salario_actual = 1500000
+        self.parametros.semanas_laboradas = 200
+        self.parametros.ahorro_pensional_a_hoy = 1000000
+        self.parametros.rentabilidad_promedio = 4
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 1000000
-        valor_pension_esperada = 300_000
+        valor_pension_esperada = 300000
+
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
 
     def test_caso_normal2(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 45
+        # Entradas
+        self.parametros.edad = 45
         sexo = "m"
         estado_civil = "c"
-        parametros.salario_actual = 3000000
-        parametros.semanas_laboradas = 1000
-        parametros.ahorro_pensional_a_hoy = 50000000
-        parametros.rentabilidad_promedio = 3
-        parametros.tasa_administracion = 1
-        # salidas esperadas
+        self.parametros.salario_actual = 3000000
+        self.parametros.semanas_laboradas = 1000
+        self.parametros.ahorro_pensional_a_hoy = 50000000
+        self.parametros.rentabilidad_promedio = 3
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 50000000
         valor_pension_esperada = 1500000
 
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
+
     def test_caso_normal3(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 28
+        # Entradas
+        self.parametros.edad = 28
         sexo = "f"
         estado_civil = "s"
-        parametros.salario_actual = 1800000
-        parametros.semanas_laboradas = 300
-        parametros.ahorro_pensional_a_hoy = 2000000
-        parametros.rentabilidad_promedio = 3
-        parametros.tasa_administracion = 1
-        # salidas esperadas
+        self.parametros.salario_actual = 1800000
+        self.parametros.semanas_laboradas = 300
+        self.parametros.ahorro_pensional_a_hoy = 2000000
+        self.parametros.rentabilidad_promedio = 3
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 2000000
         valor_pension_esperada = 600000
 
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
+
     def test_caso_normal4(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 50
+        # Entradas
+        self.parametros.edad = 50
         sexo = "f"
         estado_civil = "c"
-        parametros.salario_actual = 3500000
-        parametros.semanas_laboradas = 1200
-        parametros.ahorro_pensional_a_hoy = 75000000
-        parametros.rentabilidad_promedio = 4
-        parametros.tasa_administracion = 1
-        # salidas esperadas
+        self.parametros.salario_actual = 3500000
+        self.parametros.semanas_laboradas = 1200
+        self.parametros.ahorro_pensional_a_hoy = 75000000
+        self.parametros.rentabilidad_promedio = 4
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 75000000
         valor_pension_esperada = 2100000
 
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
+
     def test_caso_normal5(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 60
+        # Entradas
+        self.parametros.edad = 60
         sexo = "m"
         estado_civil = "c"
-        parametros.salario_actual = 4000000
-        parametros.semanas_laboradas = 1600
-        parametros.ahorro_pensional_a_hoy = 100000000
-        parametros.rentabilidad_promedio = 4
-        parametros.tasa_administracion = 1
-        # salidas esperadas
+        self.parametros.salario_actual = 4000000
+        self.parametros.semanas_laboradas = 1600
+        self.parametros.ahorro_pensional_a_hoy = 100000000
+        self.parametros.rentabilidad_promedio = 4
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 100000000
         valor_pension_esperada = 2800000
 
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
+
     def test_caso_normal6(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 58
+        # Entradas
+        self.parametros.edad = 58
         sexo = "f"
         estado_civil = "c"
-        parametros.salario_actual = 3800000
-        parametros.semanas_laboradas = 1500
-        parametros.ahorro_pensional_a_hoy = 95000000
-        parametros.rentabilidad_promedio = 3
-        parametros.tasa_administracion = 1
-        # salidas esperadas
+        self.parametros.salario_actual = 3800000
+        self.parametros.semanas_laboradas = 1500
+        self.parametros.ahorro_pensional_a_hoy = 95000000
+        self.parametros.rentabilidad_promedio = 3
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 95000000
         valor_pension_esperada = 2600000
 
+
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
+
     # casos Extraordinarios (6)
+
     def test_extraordinario1(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 78
+        # Entradas
+        self.parametros.edad = 78
         sexo = "m"
         estado_civil = "s"
-        parametros.salario_actual = 1300000
-        parametros.semanas_laboradas = 200
-        parametros.ahorro_pensional_a_hoy = 25000000
-        parametros.rentabilidad_promedio = 1
-        parametros.tasa_administracion = 2
-        # salidas esperadas: semanas laboradas menores que que 1000
+        self.parametros.salario_actual = 1300000
+        self.parametros.semanas_laboradas = 200
+        self.parametros.ahorro_pensional_a_hoy = 25000000
+        self.parametros.rentabilidad_promedio = 1
+        self.parametros.tasa_administracion = 2
+
+        # Salidas esperadas: semanas laboradas menores que 1000
+        valor_ahorro_pensional_esperado = 25000000
+        valor_pension_esperada = 0
+
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
 
     def test_extraordinario2(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 36
+        # Entradas
+        self.parametros.edad = 36
         sexo = "f"
         estado_civil = "c"
-        parametros.salario_actual = 500000
-        parametros.semanas_laboradas = 220
-        parametros.ahorro_pensional_a_hoy = 15000000
-        parametros.rentabilidad_promedio =2
-        parametros.tasa_administracion =1
-        # salidas esperadas: Su salario es menor que el SMLV
+        self.parametros.salario_actual = 500000
+        self.parametros.semanas_laboradas = 220
+        self.parametros.ahorro_pensional_a_hoy = 15000000
+        self.parametros.rentabilidad_promedio = 2
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas: Su salario es menor que el SMLV
+        valor_ahorro_pensional_esperado = 15000000
+        valor_pension_esperada = 0
+
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
 
     def test_extraordinario3(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 77
+        # Entradas
+        self.parametros.edad = 77
         sexo = "m"
         estado_civil = "s"
-        parametros.salario_actual = 200000
-        parametros.semanas_laboradas = 700
-        parametros.ahorro_pensional_a_hoy = 30000000
-        parametros.rentabilidad_promedio = 2
-        parametros.tasa_administracion = 2
-        # salidas esperadas: las semanas minimas para pensionarse son 1300
+        self.parametros.salario_actual = 200000
+        self.parametros.semanas_laboradas = 700
+        self.parametros.ahorro_pensional_a_hoy = 30000000
+        self.parametros.rentabilidad_promedio = 2
+        self.parametros.tasa_administracion = 2
+
+        # Salidas esperadas: las semanas mínimas para pensionarse son 1300
+        valor_ahorro_pensional_esperado = 30000000
+        valor_pension_esperada = 0
+
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)  # Usando un valor que fallará
 
     def test_extraordinario4(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 40
+        # Entradas
+        self.parametros.edad = 40
         sexo = "f"
         estado_civil = "s"
-        parametros.salario_actual = 1300000
-        parametros.semanas_laboradas = 1301
-        parametros.ahorro_pensional_a_hoy = 15600000
-        parametros.rentabilidad_promedio = 3
-        parametros.tasa_administracion = 2
-        # salidas esperadas: Aun no tienes la edad minima para pensionarse
+        self.parametros.salario_actual = 1300000
+        self.parametros.semanas_laboradas = 1301
+        self.parametros.ahorro_pensional_a_hoy = 15600000
+        self.parametros.rentabilidad_promedio = 3
+        self.parametros.tasa_administracion = 2
+
+        # Salidas esperadas: Aún no tienes la edad mínima para pensionarse
+        valor_ahorro_pensional_esperado = 15600000
+        valor_pension_esperada = 0
+
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
 
     def test_extraordinario5(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 76
+        # Entradas
+        self.parametros.edad = 76
         sexo = "m"
         estado_civil = "c"
-        parametros.salario_actual = 80000000
-        parametros.semanas_laboradas = 1300
-        parametros.ahorro_pensional_a_hoy = 12078720000
-        parametros.rentabilidad_promedio = 3
-        parametros.tasa_administracion = 2
-        # salidas esperadas
+        self.parametros.salario_actual = 80000000
+        self.parametros.semanas_laboradas = 1300
+        self.parametros.ahorro_pensional_a_hoy = 12078720000
+        self.parametros.rentabilidad_promedio = 3
+        self.parametros.tasa_administracion = 2
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 12078720000
         valor_pension_esperada = 56000000
 
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
+
     def test_extraordinario6(self):
-        parametros = ParametrosPension()
-        # entradas
-        parametros.edad = 62
+        # Entradas
+        self.parametros.edad = 62
         sexo = "f"
         estado_civil = "s"
-        parametros.salario_actual = 1300000
-        parametros.semanas_laboradas = 1300
-        parametros.ahorro_pensional_a_hoy = 117843200
-        parametros.rentabilidad_promedio = 2
-        parametros.tasa_administracion = 1
-        # salidas esperadas
+        self.parametros.salario_actual = 1300000
+        self.parametros.semanas_laboradas = 1300
+        self.parametros.ahorro_pensional_a_hoy = 117843200
+        self.parametros.rentabilidad_promedio = 2
+        self.parametros.tasa_administracion = 1
+
+        # Salidas esperadas
         valor_ahorro_pensional_esperado = 117843200
         valor_pension_esperada = 490930
 
-    # casos de ErROR
-    def test_error_edad_negativa(self):
-        """
-        Prueba para un caso donde la edad ingresada es negativa.
-        """
-        parametros = ParametrosPension()
-        # entradas erróneas
-        parametros.edad = -5  # Error
-        sexo = "m"
-        estado_civil = "s"
-        parametros.salario_actual = 2000000
-        parametros.semanas_laboradas = 500
-        parametros.ahorro_pensional_a_hoy = 20000000
-        parametros.rentabilidad_promedio = 4
-        parametros.tasa_administracion = 1
-        # salidas esperadas: EdadNegativaError
+        self.assertEqual(self.parametros.ahorro_pensional_a_hoy, valor_ahorro_pensional_esperado)
+        self.assertEqual(0, valor_pension_esperada)
 
+    # casos de ErROR
     def test_error_salario_negativo(self):
-        """
-        Prueba para un caso donde el salario ingresado es negativo.
-        """
         parametros = ParametrosPension()
-        # entradas erróneas
         parametros.edad = 35
-        sexo = "f"
-        estado_civil = "c"
-        parametros.salario_actual = -3000000
+        parametros.salario_actual = -3000000  # Error
         parametros.semanas_laboradas = 800
         parametros.ahorro_pensional_a_hoy = 40000000
         parametros.rentabilidad_promedio = 3
         parametros.tasa_administracion = 1
-        # salidas esperadas: SalarioNegativoError
+
+        with self.assertRaises(SalarioNegativoError):
+            parametros.validar()
 
     def test_error_semanas_laboradas_negativas(self):
-        """
-        Prueba para un caso donde las semanas laboradas ingresadas son negativas.
-        """
         parametros = ParametrosPension()
-        # entradas erróneas
         parametros.edad = 40
-        sexo = "m"
-        estado_civil = "s"
         parametros.salario_actual = 3500000
         parametros.semanas_laboradas = -100  # Error
         parametros.ahorro_pensional_a_hoy = 60000000
         parametros.rentabilidad_promedio = 4
         parametros.tasa_administracion = 1
-        # salidas esperadas: SemanasLaboradasNegativasError
+
+        with self.assertRaises(SemanasLaboradasNegativasError):
+            parametros.validar()
 
     def test_error_ahorro_pensional_negativo(self):
-        """
-        Prueba para un caso donde el ahorro pensional a hoy es negativo.
-        """
         parametros = ParametrosPension()
-        # entradas erróneas
         parametros.edad = 50
-        sexo = "f"
-        estado_civil = "c"
         parametros.salario_actual = 4000000
         parametros.semanas_laboradas = 1200
         parametros.ahorro_pensional_a_hoy = -10000000  # Error
         parametros.rentabilidad_promedio = 3
         parametros.tasa_administracion = 1
-        # salidas esperadas: AhorroPensionalNegativoError
+
+        with self.assertRaises(AhorroPensionalNegativoError):
+            parametros.validar()
 
     def test_error_rentabilidad_promedio_negativa(self):
-        """
-        Prueba para un caso donde la rentabilidad promedio es negativa.
-        """
         parametros = ParametrosPension()
-        # entradas erróneas
         parametros.edad = 55
-        sexo = "m"
-        estado_civil = "c"
         parametros.salario_actual = 5000000
         parametros.semanas_laboradas = 1500
         parametros.ahorro_pensional_a_hoy = 100000000
         parametros.rentabilidad_promedio = -2  # Error
         parametros.tasa_administracion = 1
-        # salidas esperadas: RentabilidadPromedioNegativaError
+
+        with self.assertRaises(RentabilidadPromedioNegativaError):
+            parametros.validar()
 
     def test_error_tasa_administracion_excesiva(self):
-        """
-        Prueba para un caso donde la tasa de administración excede el 3%.
-        """
         parametros = ParametrosPension()
-        # entradas erróneas
         parametros.edad = 45
-        sexo = "f"
-        estado_civil = "s"
         parametros.salario_actual = 3800000
         parametros.semanas_laboradas = 1000
         parametros.ahorro_pensional_a_hoy = 70000000
         parametros.rentabilidad_promedio = 3
         parametros.tasa_administracion = 4  # Error (excede el 3%)
-        # salidas esperadas: TasaAdministracionExcesivaError
+
+        with self.assertRaises(TasaAdministracionExcesivaError):
+            parametros.validar()
 
     def test_error_edad_muy_alta(self):
-        """
-        Prueba para un caso donde la edad ingresada es demasiado alta (por ejemplo, más de 120 años).
-        """
         parametros = ParametrosPension()
-        # entradas erróneas
         parametros.edad = 130  # Error
-        sexo = "m"
-        estado_civil = "c"
         parametros.salario_actual = 2500000
         parametros.semanas_laboradas = 1200
         parametros.ahorro_pensional_a_hoy = 50000000
         parametros.rentabilidad_promedio = 3
         parametros.tasa_administracion = 1
-        # salidas esperadas: EdadDemasiadoAltaError
+
+        with self.assertRaises(EdadDemasiadoAltaError):
+            parametros.validar()
 
     def test_error_edad_muy_baja(self):
-        """
-        Prueba para un caso donde la edad ingresada es demasiado baja (por ejemplo, menos de 16 años).
-        """
         parametros = ParametrosPension()
-        # entradas erróneas
         parametros.edad = 12  # Error
-        sexo = "f"
-        estado_civil = "s"
         parametros.salario_actual = 1200000
         parametros.semanas_laboradas = 50
         parametros.ahorro_pensional_a_hoy = 2000000
         parametros.rentabilidad_promedio = 3
         parametros.tasa_administracion = 1
-        # salidas esperadas: EdadDemasiadoBajaError
+
+        with self.assertRaises(EdadDemasiadoBajaError):
+            parametros.validar()
