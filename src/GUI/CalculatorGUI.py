89fecabd6ac94cf461e54f2d6257345c9edb5236
Edison <edison100ospina@gmail.com>
2024-09-27 06:20:58 -0500

    funcional :)

--- src/GUI/CalculatorGUI.py
+++ src/GUI/CalculatorGUI.py
@@ -10,7 +10,7 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.popup import P
+from kivy.uix.popup import Popup
 
 class CalculatorApp(App):
     def build(self):
